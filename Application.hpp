53b64dd90067b1ed3d434a10a98a527edead3660
Nikola <nikoladeyanov@proton.me>
2024-06-15 04:47:27 -0400

    feat: CLI - structure

---
+++ Application.hpp
@@ -0,0 +1,14 @@
+class Application{
+public:
+    void load(MyString s);
+    void change(MyString s);
+    void close();
+    void sessioninfo();
+    void undo();
+    void add(MyString s);
+    void save();
+    void saveAs(MyString s);
+    void collage(MyString s1, MyString s2, bool isVertical);
+    void transformation(/**???*/); //! MOD HERE
+}
+
--- CLI.hpp
+++ CLI.hpp
@@ -1,18 +1,7 @@
 #include<iostream>
-class CLI{
+class CLI : Application{
 public: 
     void run();
 protected:
-    void help();
-    void load(MyString s);
-    void change(MyString s);
-    void close();
-    void sessioninfo();
-    void undo();
-    void add(MyString s);
-    void save();
-    void saveAs(MyString s);
-    void collage(MyString s1, MyString s2, bool isVertical);
-    void transformation(/**???*/);
-    void parse(const char* cmd);
+    bool parse(const char* cmd);
 }
