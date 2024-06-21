a6cf230143114b391f37eaabacb9e42786ff929f
Nikola <nikoladeyanov@proton.me>
2024-06-21 16:27:17 +0300

    feat: parsePGM

--- parsing/parser-test.cpp
+++ parsing/parser-test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include <stdexcept>
 #include "../_string.cpp"
 using namespace std;
 
@@ -7,20 +8,28 @@ struct pixel {
     int r, g, b;
 };
 
+struct shade{
+    int v;
+};
+
 pixel** parsePlainPPM(_string filename) {
     ifstream infile(filename.c_str());
     if (!infile) {
-        cerr << "Error: Could not open file " << filename << endl;
-        return nullptr;
+        throw std::runtime_error( "Error: Could not open file " );
+        
     }
     _string magic;
     infile >> magic;
     if (magic != "P3") {
-        std::cerr << "Error: Not a valid PPM ASCII file." << std::endl;
+        throw std::runtime_error( "Error: Not a valid PPM ASCII file." );
     }
     int width, height, maxColor;
     infile >> width >> height;
     infile >> maxColor;
+    if(width == 0 || height == 0 || maxColor == 0){
+        throw std::runtime_error( "Error: Not a valid PGM ASCII file.");
+    }
+
     pixel** res = new pixel*[width];
     
     for (int x = 0; x < width; x++) { 
@@ -36,19 +45,20 @@ pixel** parsePlainPPM(_string filename) {
 pixel** parseRawPPM(_string filename) {
     ifstream infile(filename.c_str(), ios::binary);
     if (!infile) {
-        cerr << "Error: Could not open file " << filename << endl;
-        return nullptr;
+        throw std::runtime_error( "Error: Could not open file " );
     }
     _string magic;
     infile >> magic;
     if (magic != "P6") {
-        cerr << "Error: Not a valid PPM binary file." << endl;
-        return nullptr;
+        throw std::runtime_error( "Error: Not a valid PPM binary file." );
     }
+
     int width, height, maxColor;
     infile >> width >> height;
     infile >> maxColor;
-        
+    
+    // validate!!
+
     char newline;
     infile.get(newline);
     
@@ -57,7 +67,8 @@ pixel** parseRawPPM(_string filename) {
     for (int x = 0; x < width; ++x) {
         res[x] = new pixel[height];
         for (int y = 0; y < height; ++y) {
-            infile.read(reinterpret_cast<char*>(&res[x][y].r), 1);
+            //valdiare
+            infile.read(reinterpret_cast<char*>(&res[x][y].r), 1); // 1??!??!
             infile.read(reinterpret_cast<char*>(&res[x][y].g), 1);
             infile.read(reinterpret_cast<char*>(&res[x][y].b), 1);
         }
@@ -66,6 +77,70 @@ pixel** parseRawPPM(_string filename) {
     infile.close();
     return res;
 }
+
+shade** parsePlainPGM(_string filename) {
+    ifstream infile(filename.c_str());
+    if (!infile) {
+        throw std::runtime_error( "Error: Could not open file " );
+        
+    }
+    
+    _string magic;
+    infile >> magic;
+    if (magic != "P2") {
+        throw std::runtime_error( "Error: Not a valid PGM ASCII file.");
+        
+    }
+    
+    int width, height, maxShade;
+    infile >> width >> height >> maxShade;
+    // validate widht, height, maxShade
+
+    shade** res = new shade*[height];
+    
+    for (int y = 0; y < height; ++y) {
+        res[y] = new shade[width];
+        for (int x = 0; x < width; ++x) {
+            infile >> res[y][x].v;
+        }
+    }
+    
+    infile.close();
+    return res;
+}
+
+shade** parseRawPGM(_string filename) {
+    ifstream infile(filename.c_str(), ios::binary);
+    if (!infile) {
+        throw std::runtime_error( "Error: Could not open file " );
+    }
+    
+    _string magic;
+    infile >> magic;
+    if (magic != "P5") {
+        throw std::runtime_error( "Error: Not a valid PGM binary file.");
+    }
+    
+    int width, height, maxShade;
+    infile >> width >> height >> maxShade;
+
+    // validate widht, height, maxShade
+    
+    char newline;
+    infile.get(newline);
+    
+    shade** res = new shade*[height];
+    for (int y = 0; y < height; ++y) {
+        res[y] = new shade[width];
+        for (int x = 0; x < width; ++x) {
+            infile.read(reinterpret_cast<char*>(&res[y][x].v), 1); // 1??? sizes!
+        }
+    }
+    
+    infile.close();
+    return res;
+}
+
 
 
 void test1(){
@@ -86,9 +161,25 @@ void test1(){
     delete[] a;
 }
 
+void test2(){
+    shade** a = parseRawPGM("a.pgm");
+    for (int y = 0; y < 427; y++) { 
+        for (int x = 0; x < 640; x++) { 
+            cout << a[y][x].v << " ";
+        }
+        cout << endl;
+    }
+
+    
+    for (int i = 0; i < 2; i++) { 
+        delete[] a[i];
+    }
+    delete[] a;
+}
+
 int main() {
 
-    test1();
+    test2();
     
     return 0;
 }
