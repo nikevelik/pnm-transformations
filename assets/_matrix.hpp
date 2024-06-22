d0034ee0fe8ea85bcdc90e44bc602dbba30d01a9
Nikola <nikoladeyanov@proton.me>
2024-06-22 21:30:18 +0300

    matrix validation and transformations

--- assets/_matrix.hpp
+++ assets/_matrix.hpp
@@ -1,8 +1,9 @@
+#pragma once
 #include <utility>
 template <class T>
 class _matrix{
 public:
-    _matrix() = delete;
+    _matrix() = default;
     _matrix(unsigned width, unsigned height, unsigned maxvalue);
     ~_matrix();
 
@@ -14,14 +15,22 @@ public:
     unsigned width() const;
     unsigned height() const;
     unsigned maxvalue() const;
+    bool isCorrect() const;
+
+    void transpose();
+    void rotation90();
+    void rotation180();
+    void rotation270();
+    // _matrix<T>& rowcat(const _matrix<T>& o);
+    // _matrix<T>& colcat(const _matrix<T>& o);
 
     const T* operator[](unsigned idx) const;
     T* operator[](unsigned idx);
 private:
-    T** data;
-    unsigned w;
-    unsigned h;
-    unsigned mv;
+    T** data = nullptr;
+    unsigned w = 0;
+    unsigned h = 0;
+    unsigned mv = 0;
 
     void destroy();
     void copy(const _matrix<T>& o);
@@ -53,10 +62,13 @@ void _matrix<T>::move(_matrix<T>&& o){
 }
 template <class T>
 void _matrix<T>::destroy(){
-    for(unsigned y = 0; y<h; y++){
-        delete[] data[y];
+    if(data){
+        for(unsigned y = 0; y<h; y++){
+            delete[] data[y];
+        }
+        delete[] data;
+        data = nullptr;
     }
-    delete data;
 }
 template <class T>
 _matrix<T>::_matrix(unsigned width, unsigned height, unsigned maxvalue){
@@ -109,10 +121,66 @@ unsigned _matrix<T>::maxvalue() const{
     return mv;
 }
 template <class T>
+bool _matrix<T>::isCorrect() const{
+    for(unsigned y = 0; y < h; y++){
+        for(unsigned x = 0; x < w; x++){
+            if(!data[y][x].isCorrect(mv)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+template <class T>
 const T* _matrix<T>::operator[](unsigned idx) const{
     return data[idx];
 }
 template <class T>
 T* _matrix<T>::operator[](unsigned idx){
     return data[idx];
+}
+template <class T>
+void _matrix<T>::rotation90(){
+    _matrix<T> temp(h, w, mv);
+    for(unsigned y = 0; y < h; y++){
+        for(unsigned x = 0; x < w; x++){
+            temp[x][y] = data[h-1-y][x];
+        }
+    }
+    *this = std::move(temp);
+}
+
+template <class T>
+void _matrix<T>::rotation270(){
+    _matrix<T> temp(h, w, mv);
+    for(unsigned y = 0; y < h; y++){
+        for(unsigned x = 0; x < w; x++){
+            temp[x][y] = data[y][w - 1 - x];
+        }
+    }
+    *this = std::move(temp);
+}
+template <class T>
+void _matrix<T>::rotation180(){
+    unsigned midh = h/2, midw = w/2;
+    for(unsigned y = 0; y < midh; y++){
+        T* temp = data[y];
+        data[y] = data[h-1-y];
+        data[h-1-y] = temp;
+        for(unsigned x = 0; x < midw; x++){
+            T temp = data[y][x];
+            data[y][x] = data[y][w-1-x];
+            data[y][w-1-x] = temp;
+            temp = data[h-1-y][x];
+            data[h-1-y][x] = data[h-1-y][w-1-x];
+            data[h-1-y][w-1-x] = temp;
+        }
+    }
+    if(h%2==1){
+        for(unsigned x = 0; x < midw; x++){
+            T temp = data[midh][x];
+            data[midh][x] = data[midh][w-1-x];
+            data[midh][w-1-x] = temp;
+        }
+    }
 }
